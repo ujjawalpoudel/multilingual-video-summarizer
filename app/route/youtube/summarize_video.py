901892535ec145785f895c390ff9b9a0f3d047dd
Ujjawal Poudel <ujjawalpoudel@gmail.com>
2023-06-08 22:05:26 -0400

    code refactoring

--- app/route/youtube/summarize_video.py
+++ app/route/youtube/summarize_video.py
@@ -1,44 +1,42 @@
-# * Built-in modules
+# Built-in modules
 import json
 
-# * Third-party modules
+# Third-party modules
 from flask import Blueprint, request
 from mongoengine import DoesNotExist
 from pytube import YouTube
 
-
-# * Custom modules
+# Custom modules
 from utils.response_utils import response
 from service.video.youtube_metadata import get_metadata_video
 from service.video.download_youtube_video import download_video
 from service.video.video_to_audio import convert_to_audio
 
-
-# * Define Blueprint for API Routes
+# Define Blueprint for API Routes
 summarize_module = Blueprint("summarize_module", __name__)
 
 
-# * Define API Route for Login
+# Define API Route for Summarizing
 @summarize_module.route("/summarize", methods=["POST"])
 def get_summarize():
     try:
-        # * Get Data from Frontend
+        # Get data from frontend
         data = json.loads(request.data)
         url = data["url"]
 
-        # * Get MetaData from Youtube
+        # Get metadata from YouTube
         metadata = get_metadata_video(url)
 
-        # * Download Video from Youtube and get the save path
+        # Download video from YouTube and get the save path
         video_path = download_video(url)
 
-        # * Get Audio from given Video
+        # Get audio from the given video
         audio_path = convert_to_audio(video_path)
 
         return response(
             200,
             {
-                "msg": "Successfully Extracted Video",
+                "msg": "Successfully extracted video",
                 "metadata": metadata,
                 "video_path": video_path,
                 "audio_path": audio_path,
