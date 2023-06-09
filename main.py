a931155d746769d9a3b546ce4208ad0b10ff69b6
Ujjawal Poudel <ujjawalpoudel@gmail.com>
2023-06-08 22:02:18 -0400

    code reformatting

--- main.py
+++ main.py
@@ -1,38 +1,32 @@
-# * Import Python Module
+# Import Python modules
 import certifi
+import ssl
 from flask import Flask
 from mongoengine import connect
 
-# * Import User Defined Functions
+# Import user-defined functions
 from app.route.youtube.summarize_video import summarize_module
-
 from config import host_uri
 
 # Disable SSL certificate verification
-import ssl
-
 ssl._create_default_https_context = ssl._create_unverified_context
 
-# * Initialize Flask API
+# Initialize Flask API
 app = Flask(__name__)
 
 
-# * Initialize Base Flask API
+# Initialize base Flask API
 @app.route("/")
 def hello_world():
     return "Hello, User!"
 
 
-# * Register Blueprint
+# Register Blueprint
 app.register_blueprint(summarize_module, url_prefix="/youtube")
 
-# * Define the MongoDB connection
-connect(
-    host=host_uri,
-    tlsCAFile=certifi.where(),
-)
+# Define the MongoDB connection
+connect(host=host_uri, tlsCAFile=certifi.where())
 
-
-# * Run API Server
+# Run API Server
 if __name__ == "__main__":
     app.run(debug=True)
