6d2c9ae812556be7a8441977cf625a27bfa7f285
Shanover <shanoversaiyed@gmail.com>
2023-06-30 07:01:22 -0400

    segregation, cors, static serve

--- app/route/youtube/summarize_video.py
+++ app/route/youtube/summarize_video.py
@@ -3,6 +3,7 @@ import json
 
 # Third-party modules
 from flask import Blueprint, request
+from flask_cors import cross_origin
 
 # Custom modules
 from utils.response_utils import response
@@ -13,10 +14,10 @@ from service.video.audio_to_text import convert_audio_to_text
 # Define Blueprint for API Routes
 summarize_module = Blueprint("summarize_module", __name__)
 
-
-# Define API Route for Summarizing
-@summarize_module.route("/summarize", methods=["POST"])
-def summarize_video():
+# Define API Route for getting video metadata
+@summarize_module.route("/video_meta", methods=["POST"])
+@cross_origin()
+def get_video_metadata_route():
     try:
         # Get data from frontend
         data = json.loads(request.data)
@@ -25,11 +26,40 @@ def summarize_video():
         # Get metadata from YouTube
         video_metadata = get_video_metadata(url)
 
-        # Download video from YouTube and get the save path
-        # video_path = download_video(url)
+        return response(200, {"message": "Successfully extracted video metadata.", "metadata": video_metadata})
+    except Exception as e:
+        # Handle other exceptions and return an error response
+        error_message = str(e)
+        return response(401, {"message": error_message})
 
-        # Get audio from the given video
+
+# Define API Route for getting audio file URL
+@summarize_module.route("/audio_file", methods=["POST"])
+@cross_origin()
+def get_audio_file_route():
+    try:
+        # Get data from frontend
+        data = json.loads(request.data)
+        url = data["url"]
+
+        # Download video from YouTube and get the audio file URL
         audio_path = download_video_as_mp3(url)
+
+        return response(200, {"message": "Successfully obtained audio file URL.", "audio_path": audio_path})
+    except Exception as e:
+        # Handle other exceptions and return an error response
+        error_message = str(e)
+        return response(401, {"message": error_message})
+
+
+# Define API Route for summarizing video
+@summarize_module.route("/summarize", methods=["POST"])
+@cross_origin()
+def summarize_video():
+    try:
+        # Get data from frontend
+        data = json.loads(request.data)
+        audio_path = data["url"]
 
         # Get text from the given audio
         text = convert_audio_to_text(audio_path)
@@ -38,8 +68,6 @@ def summarize_video():
             200,
             {
                 "message": "Successfully extracted video metadata, audio, and text.",
-                "metadata": video_metadata,
-                "audio_path": audio_path,
                 "text": text,
             },
         )
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # Import Python modules
 import certifi
 import ssl
-from flask import Flask
+from flask import Flask, send_from_directory
 from mongoengine import connect
+from flask_cors import CORS
+import os
 
 # Import user-defined functions
 from app.route.youtube.summarize_video import summarize_module
@@ -14,12 +16,21 @@ ssl._create_default_https_context = ssl._create_unverified_context
 # Initialize Flask API
 app = Flask(__name__)
 
+# Enable CORS for all origins and allow all methods and headers
+CORS(app)
+
+# Define the path to the static directory
+static_dir = os.path.join(app.root_path, 'static')
 
 # Initialize base Flask API
 @app.route("/")
 def hello_world():
     return "Hello, User!"
 
+# Serve files from the static directory
+@app.route("/static/<path:filename>")
+def serve_static(filename):
+    return send_from_directory(static_dir, filename)
 
 # Register Blueprint
 app.register_blueprint(summarize_module, url_prefix="/youtube")
